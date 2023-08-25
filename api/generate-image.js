f93e2471faa6b3b1c2d510ea479162c29e1982bf
NexisCODES <109703201+NexisCODES@users.noreply.github.com>
2023-08-25 04:10:05 +0200

    Update generate-image.js
    updated code

--- api/generate-image.js
+++ api/generate-image.js
@@ -1,1 +1,27 @@
+const { createCanvas } = require('canvas');
+const sharp = require('sharp');
+const { parse } = require('querystring');
 
+module.exports = async (req, res) => {
+  const { hexColor } = parse(req.url.split('?')[1]);
+
+  if (!hexColor || !/^#([0-9A-F]{3}){1,2}$/i.test(hexColor)) {
+    return res.status(400).send('Invalid hex color');
+  }
+
+  const canvas = createCanvas(200, 200);
+  const context = canvas.getContext('2d');
+
+  context.fillStyle = hexColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const imageBuffer = await canvas.toBuffer();
+
+  // You can resize the image if needed using sharp
+  const resizedImageBuffer = await sharp(imageBuffer)
+    .resize(400, 400)
+    .toBuffer();
+
+  res.setHeader('Content-Type', 'image/png');
+  res.send(resizedImageBuffer);
+};
